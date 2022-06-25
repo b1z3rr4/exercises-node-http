6404bcfc8f0d40d6d6e4a4c706383a24669ce48a
b1z3rr4 <nataliasilva.bezerra18@gmail.com>
2022-06-24 22:06:11 -0300

    fisrt commit

--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
-/*
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const url = require('url');
+const host = 'localhost';
+const port = 8000;
 
-  - TODO 1: Criar um servidor que recebe requisições HTTP na porta 8000 e responde com o conteúdo de um arquivo HTML.
-  - TODO 2: Se for acessada a URL http://localhost:8000/about deve mostrar o conteúdo da pagina `pages/about.html`
-  - TODO 3: Se for acessada a URL http://localhost:8000/ ou http://localhost:8000/home deve mostrar o conteúdo da pagina `pages/index.html`
-  - TODO 4: Se for acessada qualquer outro caminho deve mostrar o conteúdo da pagina `pages/404.html`
- 
+const dirFiles = path.resolve(__dirname, 'pages');
 
-  OBS: Deve ser utilizado apenas os módulos nativos do NODE (http, path, fs, etc), nada de instalar outras libs ( ˘︹˘ )
-*/
+const routes = {
+  '/home': fs.readFileSync(dirFiles + '/index.html', 'utf-8'),
+  '/about': fs.readFileSync(dirFiles + '/about.html', 'utf-8'),
+  '404': fs.readFileSync(dirFiles + '/404.html', 'utf-8')
+}
+
+const server = http.createServer((req, res)=>{
+    const my_url = req.url;
+    const urlParse = url.parse(my_url);
+    const param = urlParse.path;
+
+    const params = {
+      '/home': '/home',
+      '/': '/home',
+      '/about': '/about',
+      'undefined': '404'
+    }
+
+    const page = routes[params[params[param]]];
+
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    res.end(page);
+});
+
+server.listen(port, host, ()=>{
+  console.log(`Server is running on https://${host}:${port}`);
+})
